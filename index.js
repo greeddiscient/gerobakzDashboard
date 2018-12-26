d3b2210469526962a65e3237d07a67ede79a50eb
Shaun Djuhari <shaundjuhari@berkeley.edu>
2018-12-26 22:16:58 +0700

    deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use('/',express.static(path.join(__dirname, 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 const PORT = process.env.PORT || 9000;
